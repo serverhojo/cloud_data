8ff8b21ad1c62158dbbe15bbe181abab39a1bf91
John Karin <103413044+serverhojo@users.noreply.github.com>
2022-04-11 07:59:56 +0000

    added host

---
+++ setup.ps1
@@ -0,0 +1,2 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
